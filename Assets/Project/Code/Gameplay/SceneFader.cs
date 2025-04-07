edacc34022983e07384afdcb6268e6617c50f057
figura8 <figuraotto@gmail.com>
2025-04-07 13:52:42 +0100

    cambiato un po di cose e iniziato capitolo 1

---
+++ Assets/Project/Code/Gameplay/SceneFader.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SceneFader : MonoBehaviour
+{
+    public Image fadeImage;
+    public float fadeDuration = 1f;
+
+    void Start()
+    {
+        // Avvia fade-in all'avvio
+        StartCoroutine(FadeIn());
+    }
+
+    public void FadeToScene(string sceneName)
+    {
+        StartCoroutine(FadeOut(sceneName));
+    }
+
+    IEnumerator FadeIn()
+    {
+        float t = 0f;
+        Color c = fadeImage.color;
+        while (t < fadeDuration)
+        {
+            t += Time.deltaTime;
+            c.a = 1f - t / fadeDuration;
+            fadeImage.color = c;
+            yield return null;
+        }
+    }
+
+    IEnumerator FadeOut(string sceneName)
+    {
+        float t = 0f;
+        Color c = fadeImage.color;
+        while (t < fadeDuration)
+        {
+            t += Time.deltaTime;
+            c.a = t / fadeDuration;
+            fadeImage.color = c;
+            yield return null;
+        }
+
+        SceneManager.LoadScene(sceneName);
+    }
+}
